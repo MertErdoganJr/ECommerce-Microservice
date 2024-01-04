e2f16d72c46e520249c10266a2e24540ce3542cb
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2024-01-04 21:51:37 +0300

    Add MongoDb and Mapper

---
+++ Services/Catalog/ECommerce.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ECommerce.Services.Catalog.Dtos.CategoryDtos;
+using ECommerce.Services.Catalog.Dtos.ProductDtos;
+using ECommerce.Services.Catalog.Models;
+
+namespace ECommerce.Services.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+
+        }
+    }
+}
