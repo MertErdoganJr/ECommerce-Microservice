6d1c16ff25d57dc1c7b320d15e749f29642c8da8
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2024-01-12 20:38:52 +0300

    Category And Product Mongo Service

--- Services/Catalog/ECommerce.Services.Catalog/Services/CategoryServices/ICategoryService.cs
+++ Services/Catalog/ECommerce.Services.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -1,6 +1,13 @@
-﻿namespace ECommerce.Services.Catalog.Services.CategoryServices
+﻿using ECommerce.Services.Catalog.Dtos.CategoryDtos;
+
+namespace ECommerce.Services.Catalog.Services.CategoryServices
 {
     public interface ICategoryService
     {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<ResultCategoryDto> GetCategoryById(string id);
     }
 }
--- Services/Catalog/ECommerce.Services.Catalog/Services/ProductServices/IProductService.cs
+++ Services/Catalog/ECommerce.Services.Catalog/Services/ProductServices/IProductService.cs
@@ -1,6 +1,13 @@
-﻿namespace ECommerce.Services.Catalog.Services.ProductServices
+﻿using ECommerce.Services.Catalog.Dtos.ProductDtos;
+
+namespace ECommerce.Services.Catalog.Services.ProductServices
 {
     public interface IProductService
     {
+        Task<List<ResultProductDto>> GetAllProductsAsync();
+        Task<ResultProductDto> GetProductsAsync(string id);
+        Task CreateProductAsync(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(string id);
     }
 }
