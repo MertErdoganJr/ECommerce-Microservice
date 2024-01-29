eb0c743adbf757fe39244f5588bb0dbb438e4a03
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2024-01-29 19:21:54 +0300

    Add Order service

--- Services/Order/Presentation/ECommerce.Order.WebApi/Program.cs
+++ Services/Order/Presentation/ECommerce.Order.WebApi/Program.cs
@@ -1,6 +1,21 @@
+using ECommerce.Order.Application.Features.CQRS.Handlers;
+using ECommerce.Order.Application.Interface;
+using ECommerce.Order.Persistance.Context;
+using ECommerce.Order.Persistance.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+});
+
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddDbContext<OrderContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
