332da54b50ba954a39ad04b5031d63a6ba5e850d
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2024-02-02 21:50:06 +0300

    Add Identity Register

--- IdentityServer/ECommerce.IdentityServer/Controllers/RegistersController.cs
+++ IdentityServer/ECommerce.IdentityServer/Controllers/RegistersController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerce.IdentityServer.Dtos;
+using ECommerce.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ECommerce.IdentityServer.Controllers
 {
@@ -7,5 +11,33 @@ namespace ECommerce.IdentityServer.Controllers
     [ApiController]
     public class RegistersController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName = userRegisterDto.UserName,
+                Email = userRegisterDto.Mail,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+                City = userRegisterDto.City
+            };
+            var result = await _userManager.CreateAsync(values,userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı Eklendi");
+            }
+            else
+            {
+                return Ok("Bir Hata Oluştu");
+            }
+        }
     }
 }
